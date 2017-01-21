23871806a446be1176d4d09f67d6f9ec5d3871a6
pjdufour <pjdufour.dev@gmail.com>
2017-01-20 21:14:20 -0500

    geolocate handler flyToCurrentLocation

---
+++ handlers/flyToCurrentLocation.js
@@ -0,0 +1,17 @@
+geodash.handlers.flyToCurrentLocation = function($scope, $interpolate, $http, $q, event, args)
+{
+    var $scope = geodash.util.getScope("geodash-main");
+    var v = geodash.var.map.getView();
+    if(navigator.geolocation)
+    {
+      navigator.geolocation.getCurrentPosition(function(position){
+        $scope.$broadcast("changeView", {
+          "lat": extract(["coords","latitude"], position, 0.0),
+          "lon": extract(["coords","longitude"], position, 0.0),
+          "zoom": 14,
+          "projection": "EPSG:4326",
+          "animations": ["pan", "bounce"]
+        });
+      });
+    }
+};
