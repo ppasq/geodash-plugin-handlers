0f05ac366b9f6ca50d033dfcbfe7955a32dad24a
Paolo Pasquali <paolo.pasquali@gmail.com>
2018-07-11 18:00:27 +0200

    New Printing functionality

--- handlers/ol3/printMap.js
+++ handlers/ol3/printMap.js
@@ -1,10 +1,7 @@
-geodash.handlers.printMap = function($scope, $interpolate, $http, $q, event, args) {
-    console.log('event', event);
-    console.log('args', args);
-    var $scope = geodash.util.getScope("geodash-main");
-    var data = geodash.var.map.getRenderer().canvas_.toDataURL("image/png")
-    //window.open(data);
-    var newWindow = window.open("", "_blank", "");
-    var html = "<a href=\""+data+"\" download=\"sparc.png\"><img src=\""+data+"\"></a>";
-    newWindow.document.write(html);
+geodash.handlers.printMap = function() {
+  $('.geodash-sidebar-header-country, #geodash-sidebar-left-charts-chart-popatrisk, #map, .geodash-map-legend-layer, .ol-scale-line').printThis({
+    canvas: true,
+    header: "<div style='float:left; height: 1.5cm; width: 2.5cm;'><img style='height: 1.5cm; margin-right: 1cm' src='/static/sparc2/build/img/en-emblem.svg'></div><div style='float:left; margin-top: 0;'><h1 style='line-height:1rem'>SPARC</h1><span style='font-size: 1rem; font-weight:300;'>Spatial Risk Calendar</span></h2></div><div style='clear:both; margin-bottom:1cm'></div>",
+    footer: "<p style='margin-top: 1cm'><img style='height: 1cm; margin-left: 0.5cm; margin-right: 1cm; margin-top: -2cm; position: fixed; right: 0; z-index: 99999' src='/static/sparc2/build/img/north_pointer.svg'><br><small>Data Sources:<br>Boundaries: WFP, UNGIWG, GAUL. Roads and settlements: OSM, GeoNames.<br>The boundaries and names and the designations used on this map do not imply official endorsement or acceptance by the United Nations.</small></p>"
+  });
 };
